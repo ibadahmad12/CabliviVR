26e2fdae317e6c163a3c6a764e415bab1ba5e7dc
Ibad Ahmad <ibad.ahmad@euphoria.pk>
2022-03-08 20:16:16 +0500

    Add icons and images

--- src/pages/Consults/index.js
+++ src/pages/Consults/index.js
@@ -3,87 +3,44 @@ import TabLayout from "../../Layouts/TabLayout";
 import "./styles.scss";
 
 export default function ConsultScreen() {
+  const doctorsData = [
+    { title: "Cardiologist" },
+    { title: "Nephrologist" },
+    { title: "Neurologist", status: "active" },
+    { title: "Hematologist", status: "active" },
+  ];
+
   return (
     <TabLayout>
       <div className="consultants-wrapper">
-        <div className="consultant-card">
-          <img
-            src={"/doctors.png"}
-            alt="person img"
-            className="person-img"
-          ></img>
-          <div className="title-wrapper">
-            <h4>Cardiologist</h4>
-            <img
-              src={
-                "https://icon-library.com/images/play-icon-png/play-icon-png-6.jpg"
-              }
-              alt="play icon"
-              className="play-img"
-            />
-          </div>
-        </div>
-        <div className="consultant-card">
-          <img
-            src={"/doctors.png"}
-            alt="person img"
-            className="person-img"
-          ></img>
-          <div className="title-wrapper">
-            <h4>Cardiologist</h4>
-            <img
-              src={
-                "https://icon-library.com/images/play-icon-png/play-icon-png-6.jpg"
-              }
-              alt="play icon"
-              className="play-img"
-            />
-          </div>
-        </div>
-        <div className="consultant-card">
-          <img
-            src={"/doctors.png"}
-            alt="person img"
-            className="person-img"
-          ></img>
-          <div className="title-wrapper">
-            <h4>Cardiologist</h4>
-            <img
-              src={
-                "https://icon-library.com/images/play-icon-png/play-icon-png-6.jpg"
-              }
-              alt="play icon"
-              className="play-img"
-            />
-          </div>
-          <img
-            src={"whiteCheck.png"}
-            alt=" select img"
-            className="selected-img"
-          />
-        </div>
-        <div className="consultant-card">
-          <img
-            src={"/doctors.png"}
-            alt="person img"
-            className="person-img"
-          ></img>
-          <div className="title-wrapper">
-            <h4>Cardiologist</h4>
-            <img
-              src={
-                "https://icon-library.com/images/play-icon-png/play-icon-png-6.jpg"
-              }
-              alt="play icon"
-              className="play-img"
-            />
-          </div>
-          <img
-            src={"/whiteCheck.png"}
-            alt=" select img"
-            className="selected-img"
-          />
-        </div>
+        {doctorsData.map(({ title, status }) => {
+          return (
+            <div className="consultant-card">
+              <img
+                src={"/doctors.png"}
+                alt="person img"
+                className="person-img"
+              ></img>
+              <div className="title-wrapper">
+                <h4>{title}</h4>
+                <img
+                  src={
+                    "https://icon-library.com/images/play-icon-png/play-icon-png-6.jpg"
+                  }
+                  alt="play icon"
+                  className="play-img"
+                />
+              </div>
+              {status && (
+                <img
+                  src={"/whiteCheck.png"}
+                  alt=" select img"
+                  className="selected-img"
+                />
+              )}
+            </div>
+          );
+        })}
       </div>
     </TabLayout>
   );
