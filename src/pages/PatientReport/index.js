1fb612604173eea73b2f0acb5af85c0f7cce6f38
Ibad Ahmad <ibad.ahmad@euphoria.pk>
2022-03-10 15:56:09 +0500

    Small UI Changes

--- src/pages/PatientReport/index.js
+++ src/pages/PatientReport/index.js
@@ -12,13 +12,27 @@ const PatientReport = () => {
         <div className="table-wrapper">
           <ClinicalReport />
           <ReportData />
-          <img src="/history.png" alt="prescription" className="report-presc" />
+          <DoctorsNotes />
           <p className="pres-footer">
             Hypothetical patient clinical laboratory report
           </p>
         </div>
       </TabLayout>
     </RootLayout>
+  );
+};
+
+const DoctorsNotes = () => {
+  return (
+    <div className="notes-container">
+      <h4>NOTES</h4>
+      <p>Not taking any meds</p>
+      <p>No history of transplant, illness, cancer, hypertension, </p>
+      <p>
+        No family history of serious illness; diabetes in uncle; grandfather
+        died of heart attack
+      </p>
+    </div>
   );
 };
 
